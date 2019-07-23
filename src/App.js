d3af1cf023b16578f06e788b45782466c1ea4117
Addiel <addielJuarez@gmail.com>
2019-07-22 21:02:35 -0500

    Vista principal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React from 'react';
+
+
+import { BrowserRouter as Router, Route, Link,Redirect } from "react-router-dom";
+
+import Principal from './principal';
+
+
+
+export default class App extends React.Component {
+  
+
+  changeView(){  
+      
+  }
+  render(){
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+            <li>
+              <Link to="/vistaNueva">Principal</Link>
+            </li>
+          </ul>
+  
+          <hr />
+  
+          <button 
+            onClick={this.changeView.bind(this)}
+          >test</button>
+  
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path='/vistaNueva' component={Principal} />
+          <Route path="/topics" component={Topics} />
+          
+        </div>
+      </Router>
+    );
+  }
+  
+}
+
+
+
+
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+
+
+
+
---
+++ src/principal.js
@@ -0,0 +1,32 @@
+/**
+ * File: /Users/addielJuarez/Documents/developer/react/navigation/src/principal.js
+ * Project: /Users/addielJuarez/Documents/developer/react/navigation
+ * Created Date: Monday, July 22nd 2019, 8:56:25 pm
+ * Author: Addiel Juarez
+ * -----
+ * Last Modified: Mon Jul 22 2019
+ * Modified By: Addiel Juarez
+ * -----
+ * Copyright (c) 2019 Addiel
+ * ------------------------------------
+ */
+
+
+
+import React from 'react';
+
+export default class Principal extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>vista Principal</h1>
+            </div>
+        );
+    }
+
+}
